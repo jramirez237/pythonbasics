bd4af3e2fe98407dacc1eabc044e001680a04bd0
Ramirez <jramirez237@cps.k12.il.us>
2024-04-29 08:41:16 -0500

    learning basics

---
+++ day 3.py
@@ -0,0 +1,28 @@
+#boolenas
+print(10>9) #is this true or false
+#booleans are used to evaluate expressions and conditions in python
+#they are used to check if an expression is true or false
+broughtfood = True
+print(broughtfood)
+
+is_raining = True
+if is_raining:
+    print("take an umbrella!")
+else: 
+    print("no umbrella needed.")
+
+temperature = 15
+if temperature >30:
+    print("it's warm")
+elif temperature >20:
+    print("it's nice")
+else:
+    print("it's cold")
+print("done")
+
+age = 22
+if age >=19:
+    message = "eligible"
+else:
+    message = "not eligible"
+print(message)                       
---
+++ main.py
@@ -0,0 +1,39 @@
+# print("hello world")
+# # print is a function that prints out a message to the console
+# # strings are surrounded by quotes
+# # single or double quotes '' or ""
+# # whenever a word is surrounded by quotes it is called a string
+# # be consistent with the quotes you use
+
+# print("your name")
+# print("order of execution")
+# print("in order")
+# print("*"*20)
+# # variables are used to store data
+# # variables are created when you assign a value to it
+# # variables are case sensitive
+
+# price = 10 #interger variable
+# name = "John" # string variable
+# rating = 4.9 #float variable
+# is_published = True #booblean variable
+# # start all variables with a lower case letter or underscore
+# print(name)
+# print(price)
+# print(rating)
+# print(is_published)
+# # string interpolation where you can use variables in a sentence
+
+# print(name + " is a basketball player")
+# # cincatention to join variables in a sentcne using the plus (+) sign
+# print (name + " has a rating of "+ str(rating))
+
+# # use the str() function to cvonvert a number to a string
+# # this is called type conversion
+# print("the price of the book is " + str(price))
+# #getting input from the user
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+occupation = input("What is your occupation? ")
+print("Hello " + name + " you are " + age + " years old and you are a " + occupation)
